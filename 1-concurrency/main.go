c490c9e6ec6e51d8774bd70c3cc9c28ef20d9b46
amyshkovskiy <a.myshkovskiy@mail.ru>
2025-05-18 19:54:51 +0300

    1-concurrency

---
+++ 1-concurrency/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+)
+
+func createSlice(wg *sync.WaitGroup, chanOfNumbers chan int) {
+	defer wg.Done()
+	sliceOfNumbers := make([]int, 10)
+	for i := 0; i < 10; i++ {
+		sliceOfNumbers[i] = rand.Intn(100)
+	}
+	for _, v := range sliceOfNumbers {
+		chanOfNumbers <- v
+	}
+	close(chanOfNumbers)
+}
+
+func sqrtNumbers(wg *sync.WaitGroup, chanOfNumbers chan int, chanSqrtNumbers chan int) {
+	defer wg.Done()
+	for val := range chanOfNumbers {
+		chanSqrtNumbers <- val * val
+	}
+	close(chanSqrtNumbers)
+
+}
+
+func main() {
+	var wg sync.WaitGroup
+	chanOfNumbers := make(chan int, 10)
+	chanSqrtNumbers := make(chan int, 10)
+	wg.Add(1)
+	go createSlice(&wg, chanOfNumbers)
+	wg.Add(1)
+	go sqrtNumbers(&wg, chanOfNumbers, chanSqrtNumbers)
+	for val := range chanSqrtNumbers {
+		fmt.Printf("%d ", val)
+	}
+	wg.Wait()
+
+}
